f8a483fbf5e35d1fd6b01976554d57ae795a0bae
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-12-31 10:21:53 +0530

    🐛 FIX: update homepage

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -53,12 +53,12 @@ export function UserMenu({ user }: UserMenuProps) {
           <DropdownMenuSeparator />
           <DropdownMenuItem asChild>
             <a
-              href="https://vercel.com"
+              href="https://100xdocs.com"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-flex items-center justify-between w-full text-xs"
             >
-              Vercel Homepage
+              100xdocs Homepage
               <IconExternalLink className="w-3 h-3 ml-auto" />
             </a>
           </DropdownMenuItem>
